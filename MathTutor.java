1b7e0c838bd992cc2d679149886adb9be19fc41c
Arora <neharora@deloitte.com>
2026-09-13 18:53:16 -0600

    I created my scanner Library and Instantiated my scanner Object and then created the print statements for the user to select a problem of their choosing. I then created a user input variable to collect what number the user typed in

---
+++ MathTutor.java
@@ -0,0 +1,44 @@
+//Math Tutor AP CSA Project
+//Ronny Jhamb
+//Instantiate a Scanner Object and use Scanner Library
+//Sources : Code.org 
+
+
+
+
+
+import java . util . Scanner;
+
+
+
+ public class MathTutor
+ {
+ public static void main (String [] args) 
+    {
+   Scanner input = new Scanner(System.in);
+    
+
+ 
+
+    
+
+    System.out.println("Welcome to Math Tutor! \n ");
+
+     System.out.println("Choose a problem: \n ");
+
+     System.out.println("1. Area of a Triangle");
+     System.out.println("2. Area of a Circle");
+     System.out.println( "3. Pythagorean Theorem \n");
+
+
+System.out.println("Enter the number of the problem you want to solve (example: 1 = Area of a Triange etc.) : ");
+int user_input = input.nextInt();
+
+
+
+     input.close();
+ 
+    }
+
+ }
+ 
